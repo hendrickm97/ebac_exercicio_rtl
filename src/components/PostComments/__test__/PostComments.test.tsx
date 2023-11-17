bf290c8eaa2c112c2892c3ae2d79d2fb5638ae00
Hendrick Menacho <hmenacho97@gmail.com>
2023-11-17 10:13:25 -0300

    adicionando comentários via teste

---
+++ src/components/PostComments/__test__/PostComments.test.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComment from "../";
+
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  it("Adicionar comentários", () => {
+    render(<PostComment />);
+
+    fireEvent.change(screen.getByTestId("comment-textarea"), {
+      target: {
+        value: "Primeiro comentário",
+      },
+    });
+    fireEvent.click(screen.getByTestId("comment-button"));
+
+    fireEvent.change(screen.getByTestId("comment-textarea"), {
+      target: {
+        value: "Segundo comentário",
+      },
+    });
+    fireEvent.click(screen.getByTestId("comment-button"));
+
+    expect(screen.getAllByTestId("comment-element")).toHaveLength(2);
+  });
+});
